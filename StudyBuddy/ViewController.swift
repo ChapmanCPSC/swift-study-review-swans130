03953d96bc0231f4a637fe89a00c650782d85725
Chase Swanstrom <chase.swanstrom@gmail.com>
2016-03-30 10:42:51 -0700

    completed

--- StudyBuddy/ViewController.swift
+++ StudyBuddy/ViewController.swift
@@ -10,9 +10,43 @@ import UIKit
 
 class ViewController: UIViewController {
 
+    @IBOutlet weak var topicLabel: UILabel!
+    @IBOutlet weak var timerLabel: UILabel!
+    
+    var count = 30
+    var random : Int = 0
+    
+    var words: [String] = ["Let v.s. Var", "Computed Initializers", "Computed Variables", "Setter Observers", "Functions", "External Parameters", "Default Parameters", "Anonymous Functions", "Optional Chaining", "Failable Initializers", "Casting", "Class Methods", "Extensions", "Enumeration", "Error Handling", "Dictionaries", "Conditional Binding", "Protocols", "Inheritance", "Structs", "Enums", "Subscripts", "Setter Observers"]
+
+    
     override func viewDidLoad() {
         super.viewDidLoad()
         // Do any additional setup after loading the view, typically from a nib.
+        
+            setTimer()
+        
+            }
+    
+    func setTimer() {
+        
+        NSTimer.scheduledTimerWithTimeInterval(1.0, target: self, selector: Selector ("update"), userInfo: nil, repeats: true)
+    }
+    
+    func update() {
+        if(count >= 0) {
+            timerLabel.text = String(count--)
+        }
+        else {
+            count = 30
+            updateWord()
+        }
+    }
+    
+    func updateWord() {
+        
+        random = Int(rand()%23)
+        topicLabel.text=String(words[random])
+        
     }
 
     override func didReceiveMemoryWarning() {
@@ -20,6 +54,11 @@ class ViewController: UIViewController {
         // Dispose of any resources that can be recreated.
     }
 
+    @IBAction func nextButton(sender: AnyObject) {
+        count = 30
+        updateWord()
+        
+    }
 
 }
 
